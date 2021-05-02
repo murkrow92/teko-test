6b59334944122effb9e4c4b69b8c67f517685215
murkrow <ducdt1992@gmail.com>
2021-05-02 16:18:35 +0700

    add transform

--- App/Containers/Product/ProductListScreen/ProductList/ProductItem.jsx
+++ App/Containers/Product/ProductListScreen/ProductList/ProductItem.jsx
@@ -8,7 +8,6 @@ import styles from './ProductItemStyles';
 function ProductItem(props) {
   const { item } = props;
   const productItem = toDisplayObject(item);
-  console.log('item:', item);
   return (
     <OpacityButton style={styles.productItemContainer}>
       <Image
--- App/Transforms/Product.js
+++ App/Transforms/Product.js
@@ -1,10 +1,20 @@
 import { formatUSDPriceToVND } from 'Utils/currencyFormatter';
 
+const discountArray = [0, 10, 20];
+
 export function toDisplayObject(product = {}, searchQuery = '') {
+  const discountPercent =
+    discountArray[Math.floor(Math.random() * discountArray.length)];
+  const originalPrice = product.price
+    ? product.price + (product.price * discountPercent) / 100
+    : 0;
+
   return {
     ...product,
     title: product.name ? product.name : '',
     displayPrice: product.price ? formatUSDPriceToVND(product.price) : '',
-    imageSource: product.imageUrl ? { uri: product.imageUrl } : { uri: '' }
+    imageSource: product.imageUrl ? { uri: product.imageUrl } : { uri: '' },
+    discountPercent: discountPercent,
+    originalPrice: formatUSDPriceToVND(originalPrice)
   };
 }
