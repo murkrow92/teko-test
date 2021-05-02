2dfe6f962bc744d67cda2059eed8b623bb88877a
murkrow <ducdt1992@gmail.com>
2021-05-02 14:54:36 +0700

    add product search bar

--- App/Containers/Product/ProductListScreen/ProductSearchBar/ProductSearchBar.jsx
+++ App/Containers/Product/ProductListScreen/ProductSearchBar/ProductSearchBar.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import { View, TextInput } from 'react-native';
 
 import Image from 'Components/FastImage/Image';
 import images from 'Themes/Images';
+import { COOL_GREY } from 'Themes/Colors';
 import styles from './ProductSearchBarStyles';
+import doNothing from 'Utils/doNothing';
 
 function ProductSearchBar(props) {
+  const { onChangeText } = props;
+
   return (
     <View style={styles.productSearchBarContainer}>
       <Image source={images.iconArrowBack} style={styles.iconArrowBack} />
       <View style={styles.searchInputContainer}>
         <Image source={images.iconSearch} style={styles.iconArrowSearch} />
+        <TextInput
+          placeholder="Nhập tên, mã sản phẩm"
+          placeholderTextColor={COOL_GREY}
+          blurOnSubmit
+          numberOfLines={1}
+          selectionColor="white"
+          underlineColorAndroid="transparent"
+          autoFocus
+          keyboardType="default"
+          style={styles.input}
+          onChangeText={onChangeText}
+        />
       </View>
     </View>
   );
 }
+
+ProductSearchBar.defaultProps = {
+  onChangeText: doNothing,
+  onDeletePress: doNothing,
+  onBackPress: doNothing
+};
+
+ProductSearchBar.propTypes = {
+  onChangeText: PropTypes.func,
+  onDeletePress: PropTypes.func,
+  onBackPress: PropTypes.func
+};
 
 export default ProductSearchBar;
