555ffff0de13f5b083adcfb66e4f35150a31cafd
murkrow <ducdt1992@gmail.com>
2021-05-02 14:12:26 +0700

    add color pallete

---
+++ App/Themes/Colors.js
@@ -0,0 +1,10 @@
+export const PALE_GREY = '#eef1f3';
+export const WHITE = '#ffffff';
+export const SILVER = '#eef1f3';
+export const TOMATO = '#ea341f';
+export const PUMPKIN_ORANGE = '#f58400';
+export const DEEP_SKY_BLUE = '#1e75f6';
+export const TOMATO_TWO = '#df2020';
+export const COOL_GREY = '#8f9598';
+export const DARK_GREY = '#262829';
+export const REDDISH_ORANGE = '#f5471e';
